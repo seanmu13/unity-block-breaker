7adffcbbca71e5a3368690117a9dc950dfd67135
Sean Murray <seanmu13@gmail.com>
2015-04-08 13:28:16 -0400

    Lecture 66 - Execution Order & Singletons

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	
+	static MusicPlayer instance = null;
+	
+	void Awake () {
+		//Debug.Log ("Music player awake " + GetInstanceID() );
+		if(instance != null) {
+			Destroy(gameObject);
+			print ("Duplicate music player self-destructing!");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+		//Debug.Log ("Music player start " + GetInstanceID() );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
