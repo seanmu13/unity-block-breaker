a592c828a8095c589c025f8671ddb820c7a59926
Sean Murray <seanmu13@gmail.com>
2015-04-08 23:17:30 -0400

    Lecture 71 - Choosing Collider Type in Unity

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class LoseCollider : MonoBehaviour {
 
+	public LevelManager levelManager;
+
 	void OnTriggerEnter2D (Collider2D trigger) {
 		print ("Trigger");
+		levelManager.LoadLevel("Win");
 	}
 	
 	void OnCollisionEnter2D (Collision2D collision) {
